699587d0a1b2411a2c54f1a3cd32d2501c3f7230
rahul <srivasrrahul@gmail.com>
2018-06-16 11:32:22 +0530

    Writing my own gradient descent

--- house_regression.py
+++ house_regression.py
@@ -4,7 +4,7 @@ from sklearn.linear_model import LinearRegression
 from sklearn import preprocessing
 from sklearn.metrics import mean_squared_error,r2_score
 import matplotlib.pyplot as plt
-
+import numpy as np
 #BASIS_COL_LST = ['bedrooms','bathrooms','sqft_living','sqft_lot','floors','waterfront','view','condition','grade']
 
 BASIS_COL_LST = ['bedrooms','bathrooms','sqft_living','sqft_lot','floors','condition','grade','sqft_above','sqft_basement','yr_built','yr_renovated','lat','long','sqft_living15','sqft_lot15']
@@ -21,12 +21,89 @@ def get_training_test_set(df,training_percent):
 
 def linear_model(train_data):
     X_train = train_data[BASIS_COL_LST]
-    print(X_train)
+    #print(X_train)
     #X_train = preprocessing.scale(X_train)
     Y_train = train_data[['price']]
     reg = LinearRegression()
     reg.fit(X_train,Y_train)
     return reg
+
+def linear_model_feature_lst(train_data,input_feature):
+    #normalize
+    #train_data[input_feature] = (train_data[input_feature]-train_data[input_feature].mean())/train_data[input_feature].std()
+    #train_data[['price']] = (train_data[['price']]-train_data[['price']].mean())/train_data[['price']].std()
+    #train_data = train_data[train_data['sqft_living'] < 2.0]
+    #train_data = train_data[train_data['price'] < 1.5]
+    X_train = train_data[input_feature]
+    #print(X_train)
+    #X_train = preprocessing.scale(X_train)
+    Y_train = train_data[['price']]
+    reg = linear_regression_manual(X_train,Y_train)
+    #reg.fit(X_train,Y_train)
+    return reg
+
+def calculate_rss(x,y,w_0,w_1):
+    s = x.shape[0]
+    #print(s)
+    diff_value = 0.0
+    #print(size)
+    for i in range(s):
+        #print(i)
+        #print(y.iloc[i,0])
+        #print(x.iloc[i,0])
+        diff_value_new = 0.0
+        diff_value_new = y.iloc[i,0]-(w_0 + w_1*x.iloc[i,0])
+        diff_value = diff_value + (diff_value_new * diff_value_new)
+
+    print("RSS is " + str(diff_value/s))
+    return diff_value/s
+
+#Gradient descent from basics
+def gradient_descent(x,y,w_0,w_1,step_size,epsilon):
+    s = x.shape[0]
+    predicted_value = calculate_rss(x,y,w_0,w_1)
+    loop_count = 0
+    while predicted_value > epsilon:
+        dim_1 = 0.0
+        dim_2 = 0.0
+        for i in range(s):
+            #print(y.iloc[i,0])
+            #print(x.iloc[i,0])
+            diff_value = y.iloc[i,0] - (w_0 + w_1*x.iloc[i,0])
+            #print(diff_value)
+            dim_1 += diff_value
+            dim_2 += diff_value * x.iloc[i, 0]
+
+        w_0_updated = w_0 + (2.0*step_size*dim_1)/s
+        w_1_updated = w_1 + (2.0*step_size*dim_2)/s
+        #print(w_0_updated)
+        #if (w_1_updated < w_1):
+        #    print("reduced1")
+        w_0 = w_0_updated
+        w_1 = w_1_updated
+        #print(w_0)
+        #print(w_1)
+        predicted_value = calculate_rss(x,y,w_0,w_1)
+        loop_count += 1
+    return w_0,w_1
+
+def linear_regression_manual(x,y):
+    x = (x-x.mean())/x.std()
+    y = (y-y.mean())/y.std()
+
+    #print(x)
+    #print(y)
+    #plt.plot(x,y)
+    #plt.show()
+    w_0, w_1 = gradient_descent(x, y, 0.0, 1.0, 0.10, 0.55)
+    #print(x)
+    #print(y)
+    plt.plot(x,y)
+    x_r = np.random.uniform(-3.0,10.0,1000)
+    y_r = w_0 + w_1*x_r
+    plt.plot(x_r,y_r)
+    plt.show()
+    return w_0,w_1
 
 
 def predict(reg,test_data):
@@ -37,6 +114,16 @@ def predict(reg,test_data):
     err = mean_squared_error(Y_test,predicted_value)
     r2 = r2_score(Y_test,predicted_value)
     return err,r2
+
+def predict_feature_lst(reg,test_data,input_feature):
+    X_test = test_data[input_feature]
+    #X_test = preprocessing.scale(X_test)
+    Y_test = test_data[['price']]
+    predicted_value = reg.predict(X_test)
+    err = mean_squared_error(Y_test,predicted_value)
+    r2 = r2_score(Y_test,predicted_value)
+    return err,r2
+
 
 
 #sqft-lot and bedrooms seem to be correlated
@@ -53,14 +140,19 @@ def test_data(df):
     #plt.scatter(df['sqft_above'],df['price'],label = 'sqft_above',color='purple')
     plt.show()
 
-df = read_file("/Users/rasrivastava/Downloads/home_data.csv")
+#df = read_file("/Users/rasrivastava/Downloads/home_data.csv")
+df = read_file("/Users/rasrivastava/Downloads/kc_house_data.csv")
 #test_data(df)
 print("Starting Training")
-train_data,test_data = get_training_test_set(df,0.9)
-print(train_data)
-reg = linear_model(train_data)
-print("End Training")
-err,r2 = predict(reg,test_data)
+train_data,test_data = get_training_test_set(df,0.80)
+#print(train_data)
+#reg = linear_model(train_data)
+reg = linear_model_feature_lst(train_data,["sqft_living"])
+print(reg)
+#print("Coefficient " + str(reg.coef_))
+#print("Intercept " + str(reg.intercept_))
+#print("End Training")
+#err,r2 = predict_feature_lst(reg,test_data,["sqft_living"])
 
-print(err)
-print(r2)
+#print(err)
+#print(r2)
